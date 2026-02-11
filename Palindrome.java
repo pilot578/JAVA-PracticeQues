3221fe96e64a2426d0c6edc1e9fe254bc479f693
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-11 13:35:54 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+int num= sc.nextInt();
+int og = num;
+int rev = 0;
+while(num>0){
+    int digit  = num % 10;
+    rev = rev*10+digit;
+    num/=10;
+}
+if(rev == og){
+    System.out.println("Yes number is palindrome");
+}
+else{
+    System.out.println("No number is not palindrome");
+}
+sc.close();
+    }   
+}
