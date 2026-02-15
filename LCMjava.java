055583be859ddf8d7218920d44a5f96592ce4dff
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-16 01:23:14 +0530

    LCM

---
+++ LCMjava.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class LCMjava{
+   static int findGCD(int a, int b){
+        while(b!=0){
+           int temp = b;
+           b = a % b;
+           a = temp;
+        }
+        return a;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+System.out.println("Enter first num: ");
+int num1 = sc.nextInt();
+System.out.println("Enter Second num: ");
+int num2 = sc.nextInt();
+int gcd = findGCD(num1, num2);
+int Lcm = (num1 * num2)/gcd;
+System.out.println("LCM IS: " + Lcm);,
+sc.close();
+    }
+}
