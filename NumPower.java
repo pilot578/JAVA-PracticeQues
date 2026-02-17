740c6b5493f366c9901beb3bf3eaccf226a82d96
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-17 23:47:25 +0530

    CalculatePower

---
+++ NumPower.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NumPower {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+int base = sc.nextInt();
+ int power = sc.nextInt();
+ int result = 1;
+ for(int i=1;i<=power;i++){
+result = result*base; 
+ }
+ System.out.println("Power of "  + base + " = " + result);
+        sc.close();
+    }
+}
