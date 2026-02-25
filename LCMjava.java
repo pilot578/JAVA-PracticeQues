eff09053199268d444134f8dd7b8c3d37a681156
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-25 23:05:44 +0530

    LCM

--- LCMjava.java
+++ LCMjava.java
@@ -10,9 +10,9 @@ public class LCMjava{
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-System.out.println("Enter first num: ");
+System.out.println("Enter first number : ");
 int num1 = sc.nextInt();
-System.out.println("Enter Second num: ");
+System.out.println("Enter Second number : ");
 int num2 = sc.nextInt();
 int gcd = findGCD(num1, num2);
 int Lcm = (num1 * num2)/gcd;
