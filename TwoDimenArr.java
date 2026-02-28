9580a977793eec58c5a62990e7b5003e8b94c9d6
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-28 22:16:23 +0530

    TwoDimensionalArray

--- TwoDimenArr.java
+++ TwoDimenArr.java
@@ -1,8 +1,8 @@
 public class TwoDimenArr {
     public static void main(String[] args){
-        String[] fruits = {"orange", "apple", "banana"};
-        String[] vegetable = {"potato", "carrot", "onion"};
-        String[] meats = {"chicken", "pork", "fish"};
+        String[] fruits = {"orange", "guava", "banana"};
+        String[] vegetable = {"potato", "carrot", "cauliflower"};
+        String[] meats = {"chicken", "egg", "fish"};
         String[][] groceries = {fruits, vegetable, meats};
         groceries[0][0] = "pineapple";
 for(String[] foods : groceries){
