42decfe562b0709555f0abcfa0a47e9d6c6195b7
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-03-02 23:50:32 +0530

    StringBuffer

---
+++ SstringBuffer.java
@@ -0,0 +1,13 @@
+public class SstringBuffer {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" Java");
+        sb.insert(5, " World");
+        sb.replace(0, 5, "Hi");
+        sb.delete(2, 3);
+        
+        System.out.println(sb);
+    }
+}
+
