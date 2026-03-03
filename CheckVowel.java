3c5727e051d779f61598b9b5add223f38b34f8f7
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-03-03 10:46:14 +0530

    StringBuffer

---
+++ CheckVowel.java
@@ -0,0 +1,14 @@
+class CheckVowel {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("AyUshh");
+        int count = 0;
+        for(int i=0;i<sb.length();i++){
+            char chh = Character.toLowerCase(sb.charAt(i));
+if(chh == 'a' || chh == 'e' || chh == 'i' || chh == 'o' || chh=='u'){
+count++;
+}
+        }
+        System.out.println("Vowels: " + count); 
+    }
+    
+}
