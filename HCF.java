d1865dd9b0d6af7d2b4a2fb087010ebad72d216d
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-03-22 23:54:40 +0530

    HCF

--- HCF.java
+++ HCF.java
@@ -10,9 +10,9 @@ public class HCF {
     }
         public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
-System.out.println("Enter num1: ");
+System.out.println("Enter number1: ");
 int num1 = sc.nextInt();
-System.out.println("Enter num2: ");
+System.out.println("Enter number2: ");
 int num2 = sc.nextInt();
 System.out.println("The HCF is: " + HCF(num1, num2));
 sc.close();
