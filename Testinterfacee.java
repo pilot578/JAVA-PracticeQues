4e29cc1983e6fc9d35059977b98ff594bc1685b7
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-03-26 23:57:39 +0530

    interface

---
+++ Testinterfacee.java
@@ -0,0 +1,22 @@
+interface Area{
+    public final float PI = 3.14f;
+    double compute(double a, double b);
+}
+
+class Rect implements Area{
+    public double compute(double a, double b){
+return a*b;
+    }
+}
+    class Circle implements Area{
+        public double compute(double a, double b){
+            return (PI*a*a);
+        }
+    }
+    class Testinterfacee{
+    public static void main(String[] args) {
+  Rect obj = new Rect();
+  System.out.println(obj.compute(2,8));      
+    
+}
+}
